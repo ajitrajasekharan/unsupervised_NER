7465bbc97668be6e8ac61d8310bd92007da26388
ajitrajasekharan <ajitrajasekharan@icloud.com>
2022-01-18 05:02:42 +0000

    minor fixes

--- fetch_labels.sh
+++ fetch_labels.sh
@@ -1,4 +1,9 @@
 mkdir -p labels 
 ( cd labels;
 gdown https://drive.google.com/uc?id=13YcDHsw2IavKbVba7vXICcLvVH1_5KWL
+tar xvf labels.tar
+mv labels/* .
+rm -rf labels
+gdown https://drive.google.com/uc?id=19E51jqh3ZQG6FayibJIcnyUOBqnFTLAl
+tar xvf server_config.tar
 )
--- fetch_models.sh
+++ fetch_models.sh
@@ -1,4 +1,5 @@
 mkdir -p models 
 ( cd models;
 gdown https://drive.google.com/uc?id=12bkVN15dBJC6OAaA3TmJ9YPlbFHQGzfJ
+tar xvf pretrained_models_for_ner.tar
 )
