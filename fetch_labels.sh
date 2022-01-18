3a92e2e74df3de12d16470af181f1e35f50bb6c6
ajitrajasekharan <ajitrajasekharan@icloud.com>
2022-01-18 01:36:33 +0000

    Easy setup scripts to fetch ensemble models and labels

---
+++ fetch_labels.sh
@@ -0,0 +1,4 @@
+mkdir -p labels 
+( cd labels;
+gdown https://drive.google.com/uc?id=13YcDHsw2IavKbVba7vXICcLvVH1_5KWL
+)
---
+++ fetch_models.sh
@@ -0,0 +1,4 @@
+mkdir -p models 
+( cd models;
+gdown https://drive.google.com/uc?id=12bkVN15dBJC6OAaA3TmJ9YPlbFHQGzfJ
+)
